c68dec04aaddb4288ecd010d1459ef9e5f866fc1
bhuvanasi <anil@live.com>
2021-11-12 14:03:47 +0530

    updated file

--- employee.java
+++ employee.java
@@ -1,3 +1,3 @@
 iam not happy with what iam now i should get nice
 Iam enter developmeent branch from master to development branch
-
+iam stash command it help in the working the other branches while we are working in master.it help as tempory storage file 
